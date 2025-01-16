9d7ec4f3a846e4d36608d144c08a4e6796b94121
BugHunter-X <askzenai@gmail.com>
2025-01-16 05:28:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+//Incorrect aggregation pipeline
+db.collection('data').aggregate([
+  {
+    $match: {status: 'active'}
+  },
+  {
+    $group: {
+      _id: '$category',
+      total: {$sum: '$value'}
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      category: '$_id',
+      total: 1
+    }
+  }
+]);
+```
